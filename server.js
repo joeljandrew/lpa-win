6f40a111868f7f3d8f2baef3273666c9757f855d
joeljandrew <joeljandrew@gmail.com>
2023-10-21 17:50:21 -0700

    update server.js to add const PORT

--- server.js
+++ server.js
@@ -80,6 +80,8 @@ app.get('/search-results', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
